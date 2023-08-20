f9a9e456d8768a385a6a27699c5efd03ee528694
Kajol Roy <shrikajol@gmail.com>
2023-08-20 19:18:24 +0600

    data modeling, apis, get product

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,42 +1,70 @@
+import dbConnect from "@/config/dbConfig";
+import productModel from "@/models/productModel";
 import { NextResponse } from "next/server";
+dbConnect();
 
 export async function GET(req) {
+  // Params from query string
+  const types = req.nextUrl.searchParams.get("types");
+  const limit = req.nextUrl.searchParams.get("limit");
+
   try {
-    return NextResponse.json(error);
+    switch (types) {
+      case "flash_sale": {
+        const data = await productModel.aggregate([
+          { $match: { status: "active", typeOfSelling: "flash_sale" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+      case "best_sale": {
+        const data = await productModel.aggregate([
+          { $match: { status: "active", typeOfSelling: "best_sale" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+      case "popular": {
+        const data = await productModel.aggregate([
+          { $match: { status: "active", typeOfSelling: "popular" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+      case "feature": {
+        const data = await productModel.aggregate([
+          { $match: { status: "active", typeOfSelling: "feature" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+      case "push": {
+        const data = await productModel.aggregate([
+          { $match: { status: "active", typeOfSelling: "push" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+      case "new": {
+        const data = await productModel.aggregate([
+          { $match: { status: "active", typeOfSelling: "new" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+      default: {
+        // default get product
+        const data = await productModel.aggregate([
+          { $match: { status: "active" } },
+          { $sample: { size: limit || 20 } },
+        ]);
+        return NextResponse.json({ success: true, products: data });
+      }
+    }
   } catch (error) {
-    return NextResponse.json(error);
-  }
-}
-
-// Method Post
-export async function POST(req) {
-  const input = await req.json();
-
-  // Protected by admin / vendor
-  if (
-    !input.title ||
-    !input.description ||
-    !input.price ||
-    !input.category ||
-    // !input.brand ||
-    !input.images ||
-    !input.thumbnail ||
-    !input.stock
-  ) {
     return NextResponse.json(
-      { success: false, message: "Invalid credentials !" },
+      { success: false, message: error.message || "Something want wrong !" },
       { status: 400 }
     );
-  }
-
-  try {
-    // Then added
-    await productModel.create({ ...input });
-    return NextResponse.json(
-      { success: true, message: "Product added successfully !" },
-      { status: 201 }
-    );
-  } catch (error) {
-    return NextResponse.json({ success: false, error }, { status: 400 });
   }
 }
