8494b6150470425403ddb090bee8f2c619f61f51
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-22 22:59:55 +0600

    account routes added

--- src/app/(site)/account/page.jsx
+++ src/app/(site)/account/page.jsx
@@ -1,7 +1,45 @@
+import userDashboardRoutes from "@/routes-data/userDashboard";
+import Link from "next/link";
+import { FaRegUserCircle } from "react-icons/fa";
 
 function Account() {
     return (
-        <div>Account</div>
+        <div>
+      <div className="mx-5 flex flex-col ">
+        <div className="flex flex-col md:flex-row justify-between items-center ">
+          <div className="flex items-center">
+            <FaRegUserCircle className="w-10 h-10"></FaRegUserCircle>
+            <div className="flex flex-col">
+              <p>Hello,</p>
+              <h1 className="text-3xl">Nahid Ahmed</h1>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="text-center mr-4">
+              <p className="border-l-2 border-r-2 w-32">Star Points</p>
+              <p className="text-orange-500 font-bold text-2xl">0</p>
+            </div>
+            <div className="text-center mr-4">
+              <p className="border-l-2 border-r-2 w-32">Star Points</p>
+              <p className="text-orange-500 font-bold text-2xl">0</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 md:grid-cols-4 gap-5 m-4">
+        {userDashboardRoutes.map((card, index) => (
+          <Link
+            href={`/account${card.href}`}
+            key={index}
+            className="group py-2 px-2 md:py-10 md:px-5 flex flex-col items-center justify-center  border rounded-md  hover:border-blue-800 shadow-lg transition-all duration-300">
+            <span className="w-11 h-11 rounded-full bg-[#EBEDF8] flex items-center justify-center mb-2">
+              {card.icon}
+            </span>
+            <p className="text-sm md:text-lg">{card.title}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
     )
 }
 
---
+++ src/routes-data/userDashboard.js
@@ -0,0 +1,45 @@
+const { AiFillStar } = require("react-icons/ai");
+const { FaShoppingCart, FaUserAlt } = require("react-icons/fa");
+const { IoMdBookmarks, IoMdWallet } = require("react-icons/io");
+
+const userDashboardRoutes = [
+      {
+        icon: <FaShoppingCart color="blue" size={20} />,
+        title: "Orders",
+        href: "/orders",
+      },
+      {
+        icon: <FaUserAlt color="blue" />,
+        title: "Edit Profile",
+        href: "/edit-profile",
+      },
+      {
+        icon: <IoMdBookmarks color="blue" size={22} />, //IoMdBookmarks
+        title: "Addresses",
+        href: "/addresses",
+      },
+      {
+        icon: <IoMdWallet color="blue" size={22} />, // GiEternalLove
+        title: "Wish list",
+        href: "/wishlist",
+      },
+  
+      {
+        icon: <AiFillStar color="blue" size={22} />,
+        title: "Start Points",
+        href: "/star-points",
+      },
+      {
+        icon: <IoMdWallet color="blue" size={22} />,
+        title: "Transactions",
+        href: "/your-transition",
+      },
+      {
+        icon: <IoMdWallet color="blue" size={22} />, // FaComputer
+        title: "Log Out",
+      },
+      // Add more card data objects here
+  ];
+
+export default userDashboardRoutes;
+
